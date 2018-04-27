08d45b8dd45172783655c74d8827410f92941a03
Micael Gallego <micael.gallego@gmail.com>
2018-04-27 18:23:21 +0200

    Fix image name ejem4

--- ejem4/build-image.sh
+++ ejem4/build-image.sh
@@ -2,4 +2,4 @@
 docker build \
  --build-arg GIT_COMMIT=$(git rev-parse HEAD)\
  --build-arg COMMIT_DATE=$(git log -1 --format=%cd --date=format:%Y-%m-%dT%H:%M:%S)\
- -t micaelgallego/curso-ci-4:latest .
+ -t micaelgallego/curso-ci-ejem4:latest .
